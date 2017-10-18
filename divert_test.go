fa9187e4b497b972e524ee201bf3583b546203e0
clmul <pclmulqdq@gmail.com>
2017-10-18 23:20:28 +0800

    refine test

--- divert_test.go
+++ divert_test.go
@@ -2,6 +2,7 @@ package divert
 
 import (
 	"bytes"
+	"context"
 	"crypto/rand"
 	"fmt"
 	"log"
@@ -10,51 +11,44 @@ import (
 	"time"
 )
 
-const (
-	n      = 19
-	length = 1234
-)
-
-var sendMsg []byte
-
-func init() {
-	sendMsg = make([]byte, n*length)
-	_, err := rand.Read(sendMsg)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func sendUDP() {
-	conn, err := net.Dial("udp", "127.0.0.8:0")
-	if err != nil {
-		log.Fatal(err)
-	}
-	for i := 0; i < n; i++ {
-		_, err = conn.Write(sendMsg[i*length : (i+1)*length])
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-}
-
-func payload(packet []byte) []byte {
-	return packet[20+8:]
-}
-
-func timeout(d time.Duration) {
-	<-time.After(d)
-	log.Fatal("timeout")
-}
-
 func TestRecv(t *testing.T) {
-	t.Log("Hello, Divert")
+	const (
+		n      = 19
+		length = 1234
+	)
+	msgs := make([]byte, n*length)
+	_, err := rand.Read(msgs)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	handle, err := Open(fmt.Sprintf("outbound and ip.DstAddr = 127.0.0.8 and udp.PayloadLength = %v", length), LayerNetwork, 0, 0)
 	if err != nil {
 		t.Fatal(err)
 	}
-	go sendUDP()
-	go timeout(time.Second * 5)
+	defer func() {
+		err = handle.Close()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	go func() {
+		conn, err := net.Dial("udp", "127.0.0.8:0")
+		if err != nil {
+			log.Fatal(err)
+		}
+		for i := 0; i < n; i++ {
+			_, err = conn.Write(msgs[i*length : (i+1)*length])
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}()
+	ctx, cancel := context.WithCancel(context.Background())
+	go timeout(ctx, time.Second*5)
+	defer cancel()
+
 	packet := make([]byte, 2048)
 	var recvMsg []byte
 	for i := 0; i < n; i++ {
@@ -62,14 +56,23 @@ func TestRecv(t *testing.T) {
 		if err != nil {
 			t.Fatal(err)
 		}
-		recvMsg = append(recvMsg, payload(packet[:n])...)
+		recvMsg = append(recvMsg, udpPayload(packet[:n])...)
 	}
-	if !bytes.Equal(recvMsg, sendMsg) {
-		t.Log(sendMsg[:20], recvMsg[:20])
+	if !bytes.Equal(recvMsg, msgs) {
+		t.Log(msgs[:20], recvMsg[:20])
 		t.Error("wrong message")
 	}
-	err = handle.Close()
-	if err != nil {
-		t.Fatal(err)
+}
+
+func udpPayload(packet []byte) []byte {
+	return packet[20+8:]
+}
+
+func timeout(ctx context.Context, d time.Duration) {
+	<-time.After(d)
+	select {
+	case <-ctx.Done():
+	default:
+		log.Fatal("timeout")
 	}
 }
